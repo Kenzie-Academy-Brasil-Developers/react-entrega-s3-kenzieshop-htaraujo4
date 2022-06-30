6fc0b1693ca9afd1be7c2937cabf811bc9ff2d98
helisson <2002130@aluno.univesp.br>
2022-06-30 10:54:30 -0300

    Envio tarefa

---
+++ src/Store/Modules/products/reducer.js
@@ -0,0 +1,69 @@
+const defaultState = [
+	{
+		id: 1,
+		name: 'Iphone 3G',
+		price: 500,
+		image:
+			'https://image.webmotors.com.br/_fotos/anunciousados/gigante/2022/202205/20220523/mclaren-720s-4.0-v8-turbo-gasolina-spider-ssg-wmimagem21494323358.jpg?s=fill&w=552&h=414&q=60',
+	},
+	{
+		id: 2,
+		name: 'Iphone 3GS',
+		price: 400,
+		image:
+			'https://image.webmotors.com.br/_fotos/anunciousados/gigante/2022/202205/20220511/ferrari-812-gts-6.5-v12-gasolina-f1dct-wmimagem18580880573.jpg?s=fill&w=552&h=414&q=60',
+	},
+	{
+		id: 3,
+		name: 'Iphone 4',
+		price: 500,
+		image:
+			'https://image.webmotors.com.br/_fotos/anunciousados/gigante/2022/202204/20220422/bmw-m3-3.0-i6-twinturbo-gasolina-competition-track-steptronic-wmimagem08133645524.jpg?s=fill&w=552&h=414&q=60',
+	},
+	{
+		id: 4,
+		name: 'Iphone 4S',
+		price: 550,
+		image:
+			'https://image.webmotors.com.br/_fotos/anunciousados/gigante/2022/202206/20220603/lamborghini-huracan-5.2-v10-gasolina-lp-640-evo-spyder-ldf-wmimagem17513064444.jpg?s=fill&w=552&h=414&q=60',
+	},
+	{
+		id: 5,
+		name: 'Iphone 5',
+		price: 600,
+		image:
+			'https://image.webmotors.com.br/_fotos/anunciousados/gigante/2022/202204/20220418/mercedesbenz-c-63-amg-4.0-v8-turbo-s-gasolina-sedan-4p-automatico-wmimagem23185028227.jpg?s=fill&w=552&h=414&q=60',
+	},
+	{
+		id: 6,
+		name: 'Iphone 5S',
+		price: 650,
+		image:
+			'https://image.webmotors.com.br/_fotos/anunciousados/gigante/2022/202204/20220418/porsche-911-3.8-6-cilindros-24v-turbo-gasolina-2p-manual-wmimagem23161313039.jpg?s=fill&w=552&h=414&q=60',
+	},
+	{
+		id: 7,
+		name: 'Iphone 6',
+		price: 700,
+		image:
+			'https://image.webmotors.com.br/_fotos/anunciousados/gigante/2022/202205/20220526/ferrari-488-spider-3.9-v8-turbo-gasolina-f1dct-wmimagem12381184019.jpg?s=fill&w=552&h=414&q=60',
+	},
+	{
+		id: 8,
+		name: 'Iphone 7',
+		price: 1000,
+		image:
+			'https://image.webmotors.com.br/_fotos/anunciousados/gigante/2022/202206/20220626/porsche-911-4.0-24v-h6-gasolina-gt3-pdk-wmimagem22254094140.jpg?s=fill&w=552&h=414&q=60',
+	},
+	{
+		id: 9,
+		name: 'Iphone 8',
+		price: 1300,
+		image:
+			'https://image.webmotors.com.br/_fotos/anunciousados/gigante/2022/202206/20220614/audi-rs4-2.9-v6-tfsi-gasolina-avant-quattro-stronic-wmimagem10250463826.jpg?s=fill&w=552&h=414&q=60',
+	},
+];
+
+export default function productsReducer(state = defaultState, action) {
+	return state;
+}
